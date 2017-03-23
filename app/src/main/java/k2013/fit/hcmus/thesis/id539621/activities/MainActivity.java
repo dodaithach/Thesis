b1f9abf705b83af11516ad90372e7c5b36e590e3
thachdd <ddthach95@gmail.com>
2017-03-23 10:20:40 +0700

    Add StreetView API.

--- app/src/main/java/k2013/fit/hcmus/thesis/id539621/activities/MainActivity.java
+++ app/src/main/java/k2013/fit/hcmus/thesis/id539621/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,20 +14,24 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
 import k2013.fit.hcmus.thesis.id539621.R;
 import k2013.fit.hcmus.thesis.id539621.sensor.OrientationCallback;
 import k2013.fit.hcmus.thesis.id539621.sensor.OrientationListener;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
     private TextView textView;
     private float[] mAngles = new float[3];
     private OrientationListener mOrientationListener;
 
     private GestureDetectorCompat mDetector;
 
-
+    private StreetViewPanorama mStreetViewPanorama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +40,23 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView)findViewById(R.id.textView);
 
-        //textView.setText("haha");
+//        mOrientationListener = new OrientationListener(this);
+//        mOrientationListener.registerListener();
+//
+//        mOrientationListener.callback = new OrientationCallback() {
+//            @Override
+//            public void onOrientationChanged() {
+//                mAngles = mOrientationListener.getOrientationAngles();
+//                textView.setText("x: " + mAngles[0] + " y: " + mAngles[1] + " z: " + mAngles[2]);
+//            }
+//        };
 
-        mOrientationListener = new OrientationListener(this);
-        mOrientationListener.registerListener();
+//        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
-        mOrientationListener.callback = new OrientationCallback() {
-            @Override
-            public void onOrientationChanged() {
-                mAngles = mOrientationListener.getOrientationAngles();
-                textView.setText("x: " + mAngles[0] + " y: " + mAngles[1] + " z: " + mAngles[2]);
-            }
-        };
-
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
@@ -65,7 +72,38 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onResume() {
         super.onResume();
+//        mOrientationListener.registerListener();
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(new LatLng(69.2079432310335, -51.16300681512197));
+        streetViewPanorama.setUserNavigationEnabled(false);
+        streetViewPanorama.setZoomGesturesEnabled(false);
+        streetViewPanorama.setStreetNamesEnabled(false);
+
+        mStreetViewPanorama = streetViewPanorama;
+
+        mOrientationListener = new OrientationListener(this);
         mOrientationListener.registerListener();
+
+        mOrientationListener.callback = new OrientationCallback() {
+            @Override
+            public void onOrientationChanged() {
+                mAngles = mOrientationListener.getOrientationAngles();
+                textView.setText("x: " + mAngles[0] + " y: " + mAngles[1] + " z: " + mAngles[2]);
+
+                float x = (float) (mAngles[0] * 180 / Math.PI);
+                float y = (float) (mAngles[1] * 180 / Math.PI);
+
+                StreetViewPanoramaCamera camera = StreetViewPanoramaCamera.builder()
+                        .bearing(x)
+                        .tilt(y)
+                        .build();
+
+                mStreetViewPanorama.animateTo(camera, 0);
+            }
+        };
     }
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
