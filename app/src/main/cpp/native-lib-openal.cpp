8b63a8a8db0c8bf1ede81c91fba132a1b047227a
Trieu Le <trieule95@gmail.com>
2017-05-21 23:34:38 +0700

    update app name

---
+++ app/src/main/cpp/native-lib-openal.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Trieu on 14/5/2017.
+//
+
---
+++ app/src/main/java/k2013/fit/hcmus/thesis/id539621/sound/OpenAL.java
@@ -0,0 +1,8 @@
+package k2013.fit.hcmus.thesis.id539621.sound;
+
+/**
+ * Created by Trieu on 14/5/2017.
+ */
+
+public class OpenAL {
+}
