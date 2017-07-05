ce7ba9f5dcad5cb9a3b50c7b51f66ef4940f38c4
thachdd <ddthach95@gmail.com>
2017-07-05 17:56:09 +0700

    Init MusicRecyclerItemClicklistener

---
+++ app/src/main/java/k2013/fit/hcmus/thesis/id539621/adapter/MusicRecyclerItemClickListener.java
@@ -0,0 +1,10 @@
+package k2013.fit.hcmus.thesis.id539621.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by cpu60011-local on 05/07/2017.
+ */
+
+public class MusicRecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+}
