cc841bc20797e4acdcb55210a877f72e67fa6178
thachdd <ddthach95@gmail.com>
2017-07-05 17:57:13 +0700

    Missing this file in last commit.

--- app/src/main/java/k2013/fit/hcmus/thesis/id539621/adapter/MusicRecyclerItemClickListener.java
+++ app/src/main/java/k2013/fit/hcmus/thesis/id539621/adapter/MusicRecyclerItemClickListener.java
@@ -1,10 +1,53 @@
 package k2013.fit.hcmus.thesis.id539621.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by cpu60011-local on 05/07/2017.
  */
 
 public class MusicRecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+    public static interface OnRecyclerItemClickListener {
+        public void onItemClick(View v, int position);
+    }
+
+    private OnRecyclerItemClickListener mListener;
+    private GestureDetector mGestureDetector;
+
+    public MusicRecyclerItemClickListener(Context context, final RecyclerView recyclerView,
+                                          OnRecyclerItemClickListener listener) {
+        mListener = listener;
+
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View child = rv.findChildViewUnder(e.getX(), e.getY());
+
+        if (child != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(child, rv.getChildPosition(child));
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
 }
